9117dbfcc876ec6c08f9c0ec8ab3e98673480f92
Erick Rampim <erickrg149@gmail.com>
2024-10-04 00:29:40 -0300

    feat: camera rework, movement complete and jump

--- scenes/player/camera.gd
+++ scenes/player/camera.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var camera_speed := 2.0
+@onready var camgimbalx := $CamGimbalX
 
 
 func  _process(delta: float) -> void:
@@ -9,8 +10,8 @@ func  _process(delta: float) -> void:
 	if Input.is_action_pressed("camera_rotation_right"):
 		rotation.y -= camera_speed * delta
 	if Input.is_action_pressed("camera_rotation_up"):
-		rotation.x += camera_speed * delta
+		camgimbalx.rotation.x += camera_speed * delta
 	if Input.is_action_pressed("camera_rotation_down"):
-		rotation.x -= camera_speed * delta
+		camgimbalx.rotation.x -= camera_speed * delta
 		
 		
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,9 +2,12 @@ extends CharacterBody3D
 
 @export var speed := 8.0
 @export var gravity := 75.0
-@export var jump_force := 7.0
+@export var jump_force := 20.0
 
 @onready var player_body = $Rig
+@onready var animation_player = $AnimationPlayer
+@onready var camera = $CamGimbalY
+
 var angular_speed := 5.0
 
 
@@ -14,22 +17,37 @@ var direction
 
 func _physics_process(delta: float) -> void:
 	move(delta)
+	attack(delta)
 
 
 func move(delta):
 	
 	movement = Input.get_vector("move_right","move_left","move_back","move_forward")
-	direction = (transform.basis * Vector3(movement.x, 0, movement.y)).normalized()
+	#direction = (transform.basis * Vector3(movement.x, 0, movement.y)).normalized()
+	direction= Vector3(movement.x, 0, movement.y).rotated(Vector3.UP, camera.rotation.y).normalized()
 	
 	if direction:
+		animation_player.play("Running_A")
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 		player_body.basis = Basis.looking_at(direction)
 	else:
+		animation_player.play("2H_Melee_Idle")
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 		
 	if not is_on_floor():
+		animation_player.play("Jump_Idle")
 		velocity.y = velocity.y - (gravity * delta)
 		
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		animation_player.play("Jump_Start")
+		velocity.y = jump_force
+
+		
 	move_and_slide()
+
+
+func attack(delta):
+	if Input.is_action_just_pressed("attack"):
+		animation_player.play("2H_Melee_Attack_Chop")
